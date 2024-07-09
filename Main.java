1adeb470e346b25cc8db52aa707c90523d08083b
Superbeq <122854822+Superbeq@users.noreply.github.com>
2024-07-09 11:23:05 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+
+        // Task 1 DONE AND TESTED
+//        Write a Java program that accept three numbers from the user and print the largest number .
+
+
+//        int num1, num2, num3, largest;
+//
+//        System.out.println("Please enter the first number: ");
+//        num1 = scan.nextInt();
+//
+//        System.out.println("Please enter the second number: ");
+//        num2 = scan.nextInt();
+//
+//        System.out.println("Please enter the third number: ");
+//        num3 = scan.nextInt();
+//
+//
+//        if(num1 > num2 && num1 > num3){
+//            largest = num1;
+//        }else if(num2 > num1 && num2 > num3){
+//            largest = num2;
+//        } else {
+//            largest = num3;
+//        }
+//
+//        System.out.println("The largest number is: " + largest);
+
+
+
+
+        // Task 2 DONE AND TESTED
+//        Write a Java program that accept a String and a number from the user,then print the character in the given index .
+
+//        String sentence;
+//        int index;
+//        System.out.println("Pleae enter a sentece: ");
+//        sentence = scan.nextLine();
+//
+//        System.out.println("Please enter an index: ");
+//        index = scan.nextInt();
+//
+//        System.out.println(sentence.charAt(index));
+
+
+
+
+        // Task 3 DONE AND TESTED
+//        Write a program to enter the numbers till the user wants and at the end it should display the sum entered .
+
+
+//        int number, sum = 0;
+//        do{
+//
+//            System.out.println("Please enter a number: (Enter 0 to quit)");
+//            number = scan.nextInt();
+//
+//                sum += number;
+//
+//        }while(number != 0);
+//
+//
+//        System.out.println("The sum of all numbers is: " + sum);
+//
+
+
+
+        // Task 4 DONE AND TESTED
+//        Write a Java program to find positive and negative numbers of a given array:
+
+
+//        int[] numbers = {10, -21, 30, 31, -25};
+//
+//        for (int n : numbers) {
+//
+//            if (n > 0) {
+//                System.out.println(n + " is a positive number");
+//            } else {
+//                System.out.println(n + " is a negative number");
+//            }
+//
+//        }
+
+
+
+
+        // Task 5 DONE AND TESTED
+//        Write a Java program to find a shortest word of a given array:
+
+
+//        String[] names = {"Tuwaiq","Bootcamp", "Student", "JAVA"};
+//        String shortTest = names[0];
+//        for(String s: names){
+//            if(s.length() < shortTest.length()){
+//                shortTest = s;
+//            }
+//        }
+//        System.out.println("The shortest one is: " + shortTest);
+//
+//
+
+    }
+}
